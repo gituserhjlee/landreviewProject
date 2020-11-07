d8ebd1aeb1e6d4d3c74b2e8dc668ff7fd874ef14
gituserhjlee <45175400+gituserhjlee@users.noreply.github.com>
2020-11-08 02:43:55 +0900

    검색필터에 따른 부동산결과보여주기

--- app.py
+++ app.py
@@ -11,6 +11,25 @@ db = client.dbmyproject
 def home():
     return render_template('index.html')
 
+@app.route('/land', methods=['POST'])
+def show_result():
+    gu, dong, name='데이터 없음','데이터 없음','데이터 없음'
+
+    filters = request.form['opt']
+    keyword = request.form['keyword']
+    if filters == '구':
+        gu = list(db.dbmyproject.find({'gu': {'$regex': keyword}}, {'_id': False}))
+        mode = 0
+    elif filters == '동':
+        dong = list(db.dbmyproject.find({'dong': {'$regex':keyword}},{'_id':False}))
+        mode = 1
+
+    else:
+        name = list(db.dbmyproject.find({'name': {'$regex': keyword}}, {'_id': False}))
+        mode = 2
+
+
+    return jsonify({'result': 'success', 'gu': gu, 'dong':dong, 'name':name, 'mode':mode})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
